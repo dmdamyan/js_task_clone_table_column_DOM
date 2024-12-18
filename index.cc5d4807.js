f588d2789f3ab59e92016164d391adf740a3c30b
Dmitrii Damyan <dmitriignc@gmail.com>
2024-12-18 22:23:10 +0200

    make build

---
+++ index.cc5d4807.js
@@ -0,0 +1,2 @@
+for(var e=document.querySelector("table"),l=0;l<e.rows.length;l++){var o=document.createElement("td"),r=e.rows[l].cells[e.rows[l].cells.length-1].cloneNode(!0);o=e.rows[l].cells[1].cloneNode(!0),e.rows[l].cells[e.rows[l].cells.length-1].replaceWith(o,r)}
+//# sourceMappingURL=index.cc5d4807.js.map
